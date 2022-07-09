fcfe9b3db3df0306aa20c3b07902beb05555fd43
Vladislav Cheremisin <wowjurneo@list.ru>
2022-07-09 03:08:44 +0300

    feat: add users graphql scripts

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,133 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+import axios from "axios";
+
+/* Move in separate files */
+
+const envError = new Error(
+  "Incorrect url to user microservice, please check .env file"
+);
+let jwt = null;
+
+type UserArgs = {
+  id: String;
+};
+
+type LoginArgs = {
+  email: String;
+  password: String;
+};
+
+type RegisterArgs = {
+  firstName: String;
+  lastName: String;
+  password: String;
+  email: String;
+};
+
+/* ---------   */
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
+  type User {
+    _id: ID!
+    firstName: String
+    lastName: String
+    password: String
+    email: String!
+  }
+
+  type Jwt {
+    jwt: String!
   }
 
   type Query {
-    books: [Book]
+    user(id: ID!): User
+    jwt(email: String!, password: String!): Jwt
+  }
+
+  type Mutation {
+    register(
+      firstName: String
+      lastName: String
+      password: String
+      email: String
+    ): User
   }
 `;
 
 const resolvers = {
   Query: {
-    books: () => books,
+    user: async (_parent: undefined, args: UserArgs) => {
+      try {
+        const url = process.env.USERS_URL + `/${args.id}`;
+        const response = await (await axios.get(url)).data;
+
+        return response;
+      } catch (err) {
+        throw new Error(
+          "Entered ID was incorrect, please try again with using correct ID!"
+        );
+      }
+    },
+
+    jwt: async (_parent: undefined, args: LoginArgs) => {
+      try {
+        const url = process.env.USERS_URL + `/login`;
+
+        if (url) {
+          const response = await (
+            await axios.post(url, {
+              email: args.email,
+              password: args.password,
+            })
+          ).data;
+
+          if (response) {
+            jwt = response.jwt;
+
+            return response;
+          } else {
+            throw new Error(
+              "Entered data was incorrect, please try again with using correct email and password!"
+            );
+          }
+        } else {
+          throw envError;
+        }
+      } catch (err) {
+        throw err;
+      }
+    },
+  },
+  Mutation: {
+    register: async (_parent: undefined, args: RegisterArgs) => {
+      try {
+        const url = process.env.USERS_URL + `/register`;
+
+        if (url) {
+          const response = await (
+            await axios.post(url, {
+              firstName: args.firstName,
+              lastName: args.lastName,
+              password: args.password,
+              email: args.email,
+            })
+          ).data;
+
+          return response;
+        } else {
+          throw envError;
+        }
+      } catch (err) {
+        if (err === envError) {
+          throw err;
+        } else {
+          throw new Error(
+            "Entered data was incorrect, please try again with using correct request data"
+          );
+        }
+      }
+    },
   },
 };
 
